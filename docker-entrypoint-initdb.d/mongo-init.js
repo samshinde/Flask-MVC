379a339ef0d22a8f93f6c168fa16b4795bd4fe09
Amol Shinde <amol.shinde@msytechnologies.com>
2020-07-29 09:13:02 +0530

    Initial commit 2.

--- docker-entrypoint-initdb.d/mongo-init.js
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -1,31 +1,4 @@
 print('Start #################################################################');
-
-db = db.getSiblingDB('auth_db');
-db.createUser(
-  {
-    user: 'mongoadmin',
-    pwd: 'password',
-    roles: [{ role: 'readWrite', db: 'auth_db' }],
-  },
-);
-db.createCollection('application_status');
-db.application_status.insertOne({
-  'status': 'alive'
-});
-
-db = db.getSiblingDB('notification_db');
-db.createUser(
-  {
-    user: 'mongoadmin',
-    pwd: 'password',
-    roles: [{ role: 'readWrite', db: 'notification_db' }],
-  },
-);
-db.createCollection('application_status');
-db.application_status.insertOne({
-  'status': 'alive'
-});
-
 db = db.getSiblingDB('entity_mgmt_db');
 db.createUser(
   {
@@ -51,5 +24,4 @@ db.createCollection('application_status');
 db.application_status.insertOne({
   'status': 'alive'
 });
-
 print('END #################################################################');
